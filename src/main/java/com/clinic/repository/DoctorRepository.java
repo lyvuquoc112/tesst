df59da03ed3494184d3fafc534fc4ccc69f7d2c0
lyvuquoc112 <lyvuquoc112@gmail.com>
2025-09-27 16:57:01 +0700

    Create DoctorRepository.java

---
+++ src/main/java/com/clinic/repository/DoctorRepository.java
@@ -0,0 +1,12 @@
+package com.clinic.repository;
+
+import com.clinic.model.Doctor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface DoctorRepository extends JpaRepository<Doctor, Long> {
+    Optional<Doctor> findByEmail(String email);
+}
